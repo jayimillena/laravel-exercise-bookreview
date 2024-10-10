0bf52e9a4f5f03c2a34e8d956279827b87547207
Jay Millena <jay.millena07@gmail.com>
2024-10-10 10:41:14 +0800

    Adding relationships

--- database/migrations/2024_10_09_111244_create_reviews_table.php
+++ database/migrations/2024_10_09_111244_create_reviews_table.php
@@ -13,8 +13,11 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('book_id');
             $table->text('review');
             $table->unsignedTinyInteger('rating');
+            $table->foreign('book_id')->references('id')->on('books')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
