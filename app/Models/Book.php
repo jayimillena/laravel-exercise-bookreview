e779250195d76d3db1f89f001fdfa6d26a1d5091
Jay Millena <jay.millena07@gmail.com>
2024-10-13 09:59:52 +0800

    Local query scopes added on model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{Model, Builder};
 
 class Book extends Model
 {
@@ -12,5 +12,10 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title  .'%');
     }
 }
